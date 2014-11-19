9ccba07986b9b97180ba9ae6bdb6144fcd81530c
RocioDSI <alu0100592832@ull.etsii.ull.es>
2014-11-19 18:52:28 +0000

    Añadiendo ruta /user

--- chat.rb
+++ chat.rb
@@ -36,3 +36,12 @@ get '/update' do
   HTML
 end
 
+get '/user' do
+  return [404, {}, "Not an ajax request"] unless request.xhr?
+  @user = user
+  erb <<-'HTML', :layout => false
+     <% @user.each do |phrase| %>
+       <%= phrase %> <br />
+     <% end %>
+  HTML
+end

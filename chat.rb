cd820e316335238f7680b4c0bcbd3dbace91e97b
RocioDSI <alu0100592832@ull.etsii.ull.es>
2014-11-20 12:31:55 +0000

    Redireccion a chat

--- chat.rb
+++ chat.rb
@@ -13,7 +13,7 @@ get '/' do
   if !session[:name]
     erb :login
   else
-    erb :index
+    erb :chat
   end 
 end
 
@@ -25,7 +25,7 @@ post '/' do
     session[:name] = name
     user << name
     puts user
-    erb :index
+    erb :chat
   end
 end
 

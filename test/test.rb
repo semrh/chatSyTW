e8445c01ff0b8fa5bf42b5ff8d817e3f36455790
Sem Ramos Herrera <semramher@gmail.com>
2014-11-21 23:29:35 +0000

    iniciando test visita a la pagina raiz

--- test/test.rb
+++ test/test.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 ENV['RACK_ENV'] = 'test'
-require_relative '../app.rb'
+require_relative '../chat.rb'
+require_relative 'test_helper.rb'
 require 'test/unit'
 require 'minitest/autorun'
 require 'rack/test'
@@ -11,4 +12,11 @@ include Rack::Test::Methods
 
 def app
    Sinatra::Application
+end
+
+describe "Tests" do
+	it "Inicio" do
+	  get '/'
+	  expect(last_response).to be_ok
+   end
 end

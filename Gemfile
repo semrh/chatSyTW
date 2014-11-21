75e9026c495524d31aff19eba41acfe0971d1dc8
RocioDSI <alu0100592832@ull.etsii.ull.es>
2014-11-21 21:21:55 +0000

    Version nueva de chat

--- Gemfile
+++ Gemfile
@@ -1,23 +1,25 @@
 source 'https://rubygems.org'
 
-ruby "2.1.3"
 
 gem 'thin'
-gem 'sinatra'
+gem "sinatra"
+gem 'omniauth'
 gem 'rack-cache'
+gem 'rest-client'
+gem 'pry'
+gem 'erubis'
 gem 'kronic'
 
-group :development, :test do
-   gem 'rack-test'
-   gem 'rake'
-   gem 'minitest'
-   gem 'test-unit'
-   gem 'selenium-webdriver'
-   gem 'coveralls', require: false
-   gem 'rspec'
-   gem 'capybara'
+group :test do
+	gem 'rack-test'
+   	gem 'rake'
+   	gem 'rspec'
+   	gem 'minitest'
+   	gem 'test-unit'
+	gem 'selenium-webdriver'
+	gem 'coveralls', require: false
 end
 
-group :production do
-   gem 'sinatra-contrib'
+group :development do
+	gem 'sinatra-contrib'
 end
--- chat.rb
+++ chat.rb
@@ -1,33 +1,65 @@
 require 'sinatra' 
 require 'sinatra/reloader' if development?
+require 'pp'
 require 'date'
 require 'kronic'
 
-#set :port, 3000
-#set :environment, :production
-
 set :server, 'thin'
 
-enable :sessions
+enable :sessions             
 set :session_secret, '*&(^#234a)'
 
-chat = ['Bienvenid@ al chat']
+chat = ['Bienvenido a la sala']
 usuarios = Array.new
 @listaUsuarios = []
 
-get '/' do 
+get ('/') do
   if @listaUsuarios == nil
-    @listadeUsuarios = usuarios
+    @listaUsuarios = usuarios
   end
-    erb :login
+  erb :index
 end
 
-post '/' do
-  session[:alias] = params[:username]
-  @usuarios = session[:alias]
-  erb :chat
+get '/send' do
+  return [404, {}, "Not an ajax request"] unless request.xhr?
+  chat << "#{session[:alias]} : #{params['text']}"
 end
 
+get '/update' do
+  return [404, {}, "Not an ajax request"] unless request.xhr?
+  @updates = chat[params['last'].to_i..-1] || []
+  @last = chat.size
+  erb <<-"HTML"
+  <% @updates.each do |phrase| %>
+  <%= phrase %> <br />
+  <% end %>
+  <span data-last="<%= @last %>"</span>
+  HTML
+
+end
+
+get '/update/usuarios' do
+  return [404, {}, "Not an ajax request"] unless request.xhr?
+  @listaUsuarios = usuarios
+  erb <<-"HTML"
+  <% @listaUsuarios.each do |usuario| %> 
+  <li class="media">
+    <div class="media-body">
+      <div class="media">
+        <div class="media-body" >
+          <h3><%= usuario %> </h3>
+          <small class="text-muted"><%= t = Time.now
+            t.to_s
+            t = t.strftime "%H:%M" + "-->" + Kronic.format(Time.now) %></small>
+          </div>
+        </div>
+      </div>
+      <hr>
+    </li>
+    <% end %>
+    HTML
+  end
+#http://www.ruby-doc.org/core-2.1.5/Array.html
 post '/registro' do
   puts "inside post '/registro/': #{params}"
   if !usuarios.include?(params[:nombre])
@@ -41,7 +73,7 @@ post '/registro' do
 end
 
 get '/logout' do
-  usuarios.delete_if { |a| a == session[:alias]}
+  usuarios.delete_if { |a| a == session[:alias] }
   session.clear
   redirect '/'
 end
@@ -54,47 +86,4 @@ get '/borrar' do
   redirect '/'
 end
 
-get '/send' do
-  return [404, {}, "Not an ajax request"] unless request.xhr?
-  chat << "#{session[:alias]}[#{t.strftime("%d/%m/%Y %H:%M:%S")}]: #{params['text']}"
-  nil
-end
 
-get '/update' do
-  return [404, {}, "Not an ajax request"] unless request.xhr?
-  @updates = chat[params['last'].to_i..-1] || []
-
-  @last = chat.size
-  erb <<-"HTML"
-      <% @updates.each do |phrase| %>
-        <%= phrase %> <br />
-      <% end %>
-      <span data-last="<%= @last %>"</span>
-  HTML
-end
-
-get '/update/usuarios' do
-  return [404, {}, "Not an ajax request"] unless request.xhr?
-  @listaUsuarios = usuarios
-  erb <<-"HTML"
-   <% @listaUsuarios.each do |usuario| %>
-     <li class="media">
-       <div class="media-body">
-         <div class="media">
-           <a class="pull-left" href="#">
-             <h3><%= usuario %> </h3>
-               <small class="text-muted"><%= t = Time.now
-               t.to_s
-               t = t.strftime "%H:%M" + "-->" + Kronic.format(Time.now) %></small>
-         </div>
-       </div>
-     </div>
-   <hr>
-   </li>
- <% end %>
- HTML
-end
-
-get '/chat' do
-  erb :chat
-end
